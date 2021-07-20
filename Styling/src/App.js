b22681569be101a5e272df76fc514d802fc944d8
youngkwon02 <jakealden97@gmail.com>
2021-07-21 02:36:32 +0900

    feat : styling/Layout

--- Styling/src/App.js
+++ Styling/src/App.js
@@ -3,16 +3,20 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {viewStyles, textStyles} from './style';
+import {Header, Contents, Footer} from './components/Layout';
 
 const App = () => {
   return (
     <View style={viewStyles.container}>
-      <Text style={[textStyles.text, {color: 'green'}]}>
+      {/* <Text style={[textStyles.text, {color: 'green'}]}>
         Inline Styling - Text
       </Text>
       <Text style={[textStyles.text, textStyles.error]}>
         Inline Styling - Error
-      </Text>
+      </Text> */}
+      <Header />
+      <Contents />
+      <Footer />
     </View>
   );
 };
