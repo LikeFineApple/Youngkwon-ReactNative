d4507627c01e47d54479ea3b5c0b2956aabdf4db
youngkwon02 <jakealden97@gmail.com>
2021-07-21 15:18:28 +0900

    feat : remove node-modules

--- Styling/src/App.js
+++ Styling/src/App.js
@@ -5,21 +5,34 @@ import {StyleSheet, View, Text} from 'react-native';
 import {viewStyles, textStyles} from './style';
 import {Header, Contents, Footer} from './components/Layout';
 import ShadowBox from './components/ShadowBox';
+import styled from 'styled-components/native';
+import Button from './components/Button';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+  align-items: center;
+  justify-content: center;
+`;
 
 const App = () => {
   return (
-    <View style={viewStyles.container}>
-      {/* <Text style={[textStyles.text, {color: 'green'}]}>
-        Inline Styling - Text
-      </Text>
-      <Text style={[textStyles.text, textStyles.error]}>
-        Inline Styling - Error
-      </Text> */}
-      {/* <Header />
-      <Contents />
-      <Footer /> */}
-      <ShadowBox />
-    </View>
+    // <View style={viewStyles.container}>
+    //   {/* <Text style={[textStyles.text, {color: 'green'}]}>
+    //     Inline Styling - Text
+    //   </Text>
+    //   <Text style={[textStyles.text, textStyles.error]}>
+    //     Inline Styling - Error
+    //   </Text> */}
+    //   {/* <Header />
+    //   <Contents />
+    //   <Footer /> */}
+    //   <ShadowBox />
+    // </View>
+    <Container>
+      <Button title="Hanbit"></Button>
+      <Button title="React Natvie"></Button>
+    </Container>
   );
 };
 
